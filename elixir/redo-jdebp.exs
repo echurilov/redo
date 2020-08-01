1507a17b8170ec0048ba39d88df749d1fd744530
Elena Churilov <elena.churilov@gmail.com>
2020-07-31 17:57:07 -0700

    Streamline redoing generation

--- elixir/redo-jdebp.exs
+++ elixir/redo-jdebp.exs
@@ -22,33 +22,37 @@ defmodule Redo do
     Path.wildcard(".redo/#{file}.{prereqs,prereqsne}.build")
     |> Enum.each(&File.rm/1)
 
-    if File.exists?("#{file}.do") do
-      redo_ifchange("#{file}.do", file)
-    else
-      default = Regex.replace(~r/.*([.][^.]*)$/, file, "default\\1.do")
-      if File.exists?("#{default}.do") do
-        redo_ifchange("#{default}.do", file)
-        redo_ifcreate("#{file}.do", file)
-      else
-        exit "cannot build #{file}: no build script (#{default}.do) found"
+    direct = "#{file}.do"
+    default = Regex.replace(~r/.*([.][^.]*)$/, file, "default\\1.do")
+	  basefile = Regex.replace(~r/\..*$/, file, "")
+    buildfile = 
+      cond do
+        File.exists?(direct) -> 
+          redo_ifchange(direct, file)
+          direct
+        File.exists?(default) ->
+          redo_ifchange(default, file)
+          redo_ifcreate(direct, file)
+          default
+        true ->
+          exit "cannot build #{file}: no build script (#{default}) found"
       end
-    end
-	  # basefile = Regex.replace(~r/\..*$/, file, "")
+
     Path.wildcard(".redo/#{file}.{uptodate,redoing}")
     |> Enum.each(&File.rm/1)
 
     "#{file}.redoing"
-    |> File.write("redoing")
+    |> File.write("./#{buildfile} #{file} #{basefile} #{file}.redoing")
 
     # if r=0 do
   end
 
-  def redo_ifchange(buildfile, file) do
-    IO.inspect("ifchange: #{buildfile}, #{file}")
+  def redo_ifchange(buildfile, redoparent) do
+    IO.inspect("ifchange: #{buildfile}, #{redoparent}")
   end
 
-  def redo_ifcreate(buildfile, file) do
-    IO.inspect("ifcreate: #{buildfile}, #{file}")
+  def redo_ifcreate(buildfile, redoparent) do
+    IO.inspect("ifcreate: #{buildfile}, #{redoparent}")
   end
 end
 
